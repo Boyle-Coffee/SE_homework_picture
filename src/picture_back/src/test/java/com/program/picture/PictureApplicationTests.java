05f947c5d016febf554747ebe65afa0d4b7acb4f
YaYaHuang <570911275@qq.com>
2020-11-07 15:18:38 +0800

    功能: 初始化后台项目架构

---
+++ src/picture_back/src/test/java/com/program/picture/PictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.picture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
