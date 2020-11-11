f3b53007f4a7fa700c681822510cfe5d98d260f2
Boyle <2018770887@qq.com>
2020-11-11 10:57:11 +0800

    文档：更新接口文档以及部分代码实现

--- src/picture_ai/picture_ai/views.py
+++ src/picture_ai/picture_ai/views.py
@@ -1,5 +1,18 @@
 # author:Boyle time:2020/11/6
 from django.http import HttpResponse
 
+
 def hello_world(request):
     return HttpResponse("Hello world!")
+
+
+def image_insert_view(request):
+    pass
+
+
+def image_search_view(request):
+    pass
+
+
+def image_recognition_view(request):
+    pass
