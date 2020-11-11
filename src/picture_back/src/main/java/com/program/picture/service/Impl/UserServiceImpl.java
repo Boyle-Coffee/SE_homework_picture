d2ec336bcaf90f03a761b94c063bd66a8689cd7e
bluebear4 <347278372@qq.com>
2020-11-11 23:08:36 +0800

    功能：增加了用户喜欢标签的接口 修改了一下之前的用户接口

--- src/picture_back/src/main/java/com/program/picture/service/Impl/UserServiceImpl.java
+++ src/picture_back/src/main/java/com/program/picture/service/Impl/UserServiceImpl.java
@@ -5,13 +5,17 @@ import com.program.picture.common.result.ResultCodeEnum;
 import com.program.picture.domain.entity.User;
 import com.program.picture.domain.entity.UserDetails;
 import com.program.picture.domain.entity.UserFollow;
+import com.program.picture.domain.entity.UserLikeType;
 import com.program.picture.mapper.UserDetailsMapper;
 import com.program.picture.mapper.UserFollowMapper;
+import com.program.picture.mapper.UserLikeTypeMapper;
 import com.program.picture.mapper.UserMapper;
 import com.program.picture.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -32,30 +36,41 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserFollowMapper userFollowMapper;
 
+    @Autowired
+    private UserLikeTypeMapper userLikeTypeMapper;
+
     @Override
-    public ResultCodeEnum userRegister(User user) {
+    public ResultCodeEnum userRegister(String userName, String password) {
         List<User> users = userMapper.selectAll();
-        for (User it : users) {
-            if (it.getName().equals(user.getName())) {
+        for (User user : users) {
+            if (user.getName().equals(userName)) {
                 return ResultCodeEnum.User_Exists_Exception;
             }
         }
-        user.setCreateTime(new Date());
+
+        User user = User.builder()
+                .name(userName)
+                .password(password)
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
         userMapper.insert(user);
 
-        UserDetails details = new UserDetails();
-        details.setCreateTime(new Date());
+        UserDetails details = UserDetails.builder()
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
         userDetailsMapper.insert(details);
 
         return ResultCodeEnum.SUCCESS;
     }
 
     @Override
-    public ResultCodeEnum userLogin(User user) {
+    public ResultCodeEnum userLogin(String userName, String password) {
         List<User> users = userMapper.selectAll();
-        for (User it : users) {
-            if (it.getName().equals(user.getName())) {
-                if (it.getPassword().equals(user.getPassword())) {
+        for (User user : users) {
+            if (user.getName().equals(userName)) {
+                if (user.getPassword().equals(password)) {
                     return ResultCodeEnum.SUCCESS;
                 } else {
                     return ResultCodeEnum.User_Login_Fail;
@@ -65,6 +80,34 @@ public class UserServiceImpl implements UserService {
         return ResultCodeEnum.User_Not_Exists_Exception;
     }
 
+    @Override
+    public ResultCodeEnum userChangePassword(String userName, String oldPassword, String newPassword) {
+        List<User> users = userMapper.selectAll();
+        for (User user : users) {
+            if (user.getName().equals(userName)) {
+                if (user.getPassword().equals(oldPassword)) {
+                    user.setPassword(newPassword);
+                    user.setUpdateTime(new Date());
+                    userMapper.updateByPrimaryKey(user);
+                    return ResultCodeEnum.SUCCESS;
+                } else {
+                    return ResultCodeEnum.User_Change_Password_Fail;
+                }
+            }
+        }
+        return ResultCodeEnum.User_Not_Exists_Exception;
+    }
+
+    @Override
+    public UserDetails userGetDetails(Integer userId) {
+        List<UserDetails> userDetails = userDetailsMapper.selectAll();
+        for (UserDetails userDetail : userDetails) {
+            if (userDetail.getUserId().equals(userId)) {
+                return userDetail;
+            }
+        }
+        return null;
+    }
 
     @Override
     public ResultCodeEnum userChangeDetails(UserDetails details) {
@@ -75,44 +118,31 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDetails userGetDetails(User user) {
-        List<User> users = userMapper.selectAll();
-        for (User it : users) {
-            if (it.getName().equals(user.getName())) {
-                List<UserDetails> userDetails = userDetailsMapper.selectAll();
-                for (UserDetails userDetail : userDetails) {
-                    if (userDetail.getUserId().equals(user.getId())) {
-                        return userDetail;
-                    }
-                }
-            }
-        }
-        return null;
-    }
+    public ResultCodeEnum userFollow(Integer userId, Integer followId) {
 
-    @Override
-    public ResultCodeEnum userFollow(User from, User to) {
         List<UserFollow> userFollows = userFollowMapper.selectAll();
         for (UserFollow userFollow : userFollows) {
-            if (userFollow.getUserId().equals(from.getId()) && userFollow.getFollowId().equals(to.getId())) {
+            if (userFollow.getUserId().equals(userId) && userFollow.getFollowId().equals(followId)) {
                 return ResultCodeEnum.User_Follow_Fail;
             }
         }
-        UserFollow userFollow = new UserFollow();
-        userFollow.setCreateTime(new Date());
-        userFollow.setUpdateTime(new Date());
-        userFollow.setUserId(from.getId());
-        userFollow.setFollowId(to.getId());
 
+        UserFollow userFollow = UserFollow.builder()
+                .userId(userId)
+                .followId(followId)
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
         userFollowMapper.insert(userFollow);
         return ResultCodeEnum.SUCCESS;
     }
 
     @Override
-    public ResultCodeEnum userFollowCancel(User from, User to) {
+    public ResultCodeEnum userFollowCancel(Integer userId, Integer followId) {
+
         List<UserFollow> userFollows = userFollowMapper.selectAll();
         for (UserFollow userFollow : userFollows) {
-            if (userFollow.getUserId().equals(from.getId()) && userFollow.getFollowId().equals(to.getId())) {
+            if (userFollow.getUserId().equals(userId) && userFollow.getFollowId().equals(followId)) {
                 userFollowMapper.deleteByPrimaryKey(userFollow.getId());
                 return ResultCodeEnum.SUCCESS;
             }
@@ -120,5 +150,46 @@ public class UserServiceImpl implements UserService {
         return ResultCodeEnum.User_Follow_Cancel_Fail;
     }
 
+    @Override
+    public ResultCodeEnum userAddLikeType(Integer userId, Integer typeId) {
+        List<UserLikeType> userLikeTypes = userLikeTypeMapper.selectAll();
+        for (UserLikeType userLikeType : userLikeTypes) {
+            if (userLikeType.getUserId().equals(userId) && userLikeType.getTypeId().equals(typeId)) {
+                return ResultCodeEnum.User_Add_Type_Fail;
+            }
+        }
+        UserLikeType userLikeType = UserLikeType.builder()
+                .userId(userId)
+                .typeId(typeId)
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
+        userLikeTypeMapper.insert(userLikeType);
+        return ResultCodeEnum.SUCCESS;
+    }
+
+    @Override
+    public List<Integer> userGetLikeType(Integer userId) {
+        List<Integer> result = new ArrayList<>();
+        List<UserLikeType> userLikeTypes = userLikeTypeMapper.selectAll();
+        for (UserLikeType userLikeType : userLikeTypes) {
+            if (userLikeType.getUserId().equals(userId)) {
+                result.add(userLikeType.getTypeId());
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public ResultCodeEnum userDeleteLikeType(Integer userId, Integer typeId) {
+        List<UserLikeType> userLikeTypes = userLikeTypeMapper.selectAll();
+        for (UserLikeType userLikeType : userLikeTypes) {
+            if (userLikeType.getUserId().equals(userId) && userLikeType.getTypeId().equals(typeId)) {
+                userLikeTypeMapper.deleteByPrimaryKey(userLikeType.getId());
+                return ResultCodeEnum.SUCCESS;
+            }
+        }
+        return ResultCodeEnum.User_Del_Type_Fail;
+    }
 
 }
--- src/picture_back/src/main/java/com/program/picture/service/UserService.java
+++ src/picture_back/src/main/java/com/program/picture/service/UserService.java
@@ -4,21 +4,31 @@ import com.program.picture.common.result.ResultCodeEnum;
 import com.program.picture.domain.entity.User;
 import com.program.picture.domain.entity.UserDetails;
 
+import java.util.List;
+
 /**
  * @auther: Blue bear
  * @date: 2020/11/09/18:50
  * @description: 用户的操作接口
  */
 public interface UserService {
-    ResultCodeEnum userRegister(User user);
+    ResultCodeEnum userRegister(String userName,String password);
 
-    ResultCodeEnum userLogin(User user);
+    ResultCodeEnum userLogin(String userName,String password);
+
+    ResultCodeEnum userChangePassword(String userName,String oldPassword,String newPassword);
 
     ResultCodeEnum userChangeDetails(UserDetails details);
 
-    UserDetails userGetDetails(User user);
+    UserDetails userGetDetails(Integer userId);
 
-    ResultCodeEnum userFollow(User from, User to);
+    ResultCodeEnum userFollow(Integer userId, Integer followId);
 
-    ResultCodeEnum userFollowCancel(User from, User to);
+    ResultCodeEnum userFollowCancel(Integer userId, Integer followId);
+
+    ResultCodeEnum userAddLikeType(Integer userId, Integer typeId);
+
+    List<Integer> userGetLikeType(Integer userId);
+
+    ResultCodeEnum userDeleteLikeType(Integer userId, Integer typeId);
 }
